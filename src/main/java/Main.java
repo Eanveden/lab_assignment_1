752c97feef9a28b808f8844035aba5ba15994987
eanveden <eanveden@kth.se>
2021-02-04 14:29:24 -0500

    Add input file and basic main file

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("input.txt"));
+        int[] integers = new int[8];
+        int i = 0;
+        while (scan.hasNextInt())
+            integers[i++] = scan.nextInt();
+    }
+}
