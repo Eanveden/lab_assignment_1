cc47ce17a709749c9fcdaf5fb52102227a490884
eanveden <eanveden@kth.se>
2021-02-12 03:12:52 -0500

    Add Multithreading/tree aspect to code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Arrays;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -9,23 +8,27 @@ public class Main {
      * Main Method for scanning and initiating mergesort
      *
      * @param args command line args
-     * @throws FileNotFoundException if input.txt is not in classpath/does not exist.
+     * @throws IOException          if input.txt is not in classpath/does not exist, or if there is a problem creating output.txt
+     * @throws InterruptedException if unexpected interruption occur during seperate running threads.
      */
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
         //Scan Input file, and copy integers from input.txt to integers array
         Scanner scan = new Scanner(new File("input.txt"));
         int[] integers = new int[8];
         int iterator = 0;
         while (scan.hasNextInt()) integers[iterator++] = scan.nextInt();
 
-        //Print Values prior to sorting
-        System.out.println(String.format("Value Before Sort: %s ", Arrays.toString(integers)));
+        //Create or Replace Outputfile if it does not already exist
+        File outputFile = new File("output.txt");
+        if (outputFile.exists()) outputFile.delete();
+        outputFile.createNewFile();
 
-        //initialize mergesort
-        MergeSort.sort(integers, iterator);
+        //Start thread number 1 to initalize the tree
+        MultiThreadMergeSort multiThreadMergeSort = new MultiThreadMergeSort(integers, integers.length, "", true);
+        multiThreadMergeSort.start();
+        multiThreadMergeSort.join();
 
-        //Print Values after sorting
-        System.out.println(String.format("Value Before Sort: %s ", Arrays.toString(integers)));
     }
 
 }
--- src/main/java/MultiThreadMergeSort.java
+++ src/main/java/MultiThreadMergeSort.java
@@ -1,28 +1,71 @@
-import javafx.concurrent.Worker;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
-public class multiThreadMergeSort extends Thread {
+public class MultiThreadMergeSort extends Thread {
+    private static final Logger DEBUGGER = Logger.getLogger("DEBUG");
+    private int[] array;
+    private int[] result;
+    private int arraySize;
+    private StringBuilder leaf;
+
+    MultiThreadMergeSort(int[] array, int arraySize, String leaf, boolean isRight) {
+        this.array = array;
+        this.result = array;
+        this.arraySize = arraySize;
+        this.leaf = new StringBuilder(String.format("%s%s", leaf, isRight ? "1" : "0"));
+    }
 
     /**
-     * merge sort method, sort int[] array of size arraySize. For example,if input array is [2,3,1] then the sorted
-     * array would be [1,2,3]
+     * Write to output.txt file
      *
-     * @param array     of integers to be sorted
-     * @param arraySize size of integer array to be sorted
+     * @param outputLine text to write
      */
-    public static void sort(int[] array, int arraySize) {
+    private void writeToOutputFile(String outputLine) {
+        try {
+            FileWriter writer = new FileWriter("output.txt", true);
+            writer.write(outputLine);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        writeToOutputFile(String.format("Thread %s started \n", leaf.toString()));
+        sort();
+        writeToOutputFile(String.format("Thread %s finished: %s \n", leaf.toString(), Arrays.toString(result)));
+    }
+
+    /**
+     * Sorts integer array recursively using merge sort. Each split will initialize a new thread
+     * -> not efficient, but fun :)
+     */
+    private void sort() {
         if (arraySize == 1) return;
         int middleIndex = arraySize / 2;
-
         int[] leftArray = new int[middleIndex];
         int[] rightArray = new int[arraySize - middleIndex];
 
         System.arraycopy(array, 0, leftArray, 0, middleIndex);
         System.arraycopy(array, middleIndex, rightArray, 0, arraySize - middleIndex);
 
-        sort(leftArray, middleIndex);
-        sort(rightArray, arraySize - middleIndex);
+        MultiThreadMergeSort sortLeft = new MultiThreadMergeSort(leftArray, leftArray.length, leaf.toString(), false);
+        sortLeft.start();
+        MultiThreadMergeSort sortRight = new MultiThreadMergeSort(rightArray, rightArray.length, leaf.toString(), true);
+        sortRight.start();
 
-        merge(array, leftArray, rightArray, middleIndex, arraySize - middleIndex);
+        try {
+            sortLeft.join();
+            sortRight.join();
+        } catch (Exception e) {
+            DEBUGGER.log(Level.SEVERE, String.format("message: %s", e.getMessage()));
+        }
+
+        merge(result, sortLeft.result, sortRight.result, middleIndex, arraySize - middleIndex);
     }
 
     /**
@@ -34,7 +77,7 @@ public class multiThreadMergeSort extends Thread {
      * @param leftIndex  left index of array, ex: 2
      * @param rightIndex right side index of array, ex: 2
      */
-    private static void merge(int[] array, int[] leftArray, int[] rightArray, int leftIndex, int rightIndex) {
+    private void merge(int[] array, int[] leftArray, int[] rightArray, int leftIndex, int rightIndex) {
         int i = 0, j = 0, k = 0;
 
         while (i < leftIndex && j < rightIndex) {
@@ -44,6 +87,5 @@ public class multiThreadMergeSort extends Thread {
 
         while (i < leftIndex) array[k++] = leftArray[i++];
         while (j < rightIndex) array[k++] = rightArray[j++];
-
     }
 }
